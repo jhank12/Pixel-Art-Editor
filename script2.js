6fe341b538eec4301b24e3a1223beab605c55ac2
Josh Hankins <jhankins0903@gmail.com>
2022-08-01 20:20:51 -0400

    updated js to set grid lines on background canvas

--- script2.js
+++ script2.js
@@ -5,4 +5,45 @@
 const backgroundCanvas = document.getElementById('backgroundCanvas');
 const contentCanvas = document.getElementById('contentCanvas');
 
-console.log(backgroundCanvas, contentCanvas)
+const ctxBg = backgroundCanvas.getContext('2d');
+const ctxContent = contentCanvas.getContext('2d');
+
+// used one canvas to get dimensions
+const canvasWidth = contentCanvas.width;
+const canvasHeight = contentCanvas.height;
+
+let cellSize;
+let gridSize;
+
+setGrid()
+// grid lines only being set on bg canvas
+
+function setGrid(e) {
+   
+   // ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+   gridSize = 8;
+   cellSize = (canvasWidth / gridSize);
+
+   
+   // gridSize = 8;
+
+   cellSize = (canvasWidth / gridSize);
+   // console.log(cellSize);
+   let offset = cellSize;
+
+   for (let i = 1; i <= gridSize; i++) {
+      // let offset = cellSize;
+      ctxBg.beginPath();
+      // x axis
+      ctxBg.moveTo(0, offset);
+      ctxBg.lineTo(canvasWidth, offset);
+      // y axis
+      ctxBg.moveTo(offset, 0);
+      ctxBg.lineTo(offset, canvasHeight);
+      ctxBg.strokeStyle = '#424242';
+      ctxBg.stroke();
+      
+      offset = cellSize * i;
+   }
+   
+}
