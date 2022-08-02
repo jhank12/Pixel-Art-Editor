1aaa639aeba61d8f7e3376a4c085068796d68208
Josh Hankins <jhankins0903@gmail.com>
2022-08-01 20:09:29 -0400

    updated index, styles and script2

--- script2.js
+++ script2.js
@@ -3,3 +3,6 @@
 // get all canvas items for both (context, width and height)
 
 const backgroundCanvas = document.getElementById('backgroundCanvas');
+const contentCanvas = document.getElementById('contentCanvas');
+
+console.log(backgroundCanvas, contentCanvas)
